7b60cd2db45db092969eb72beecf1a2d600ea247
Matthew Hoffman <mhoffman@lanl.gov>
2026-06-12 22:00:01 -0700

    Update MALI build so compile with FastIsostasy succeeds

--- components/mpas-albany-landice/src/build_options.mk
+++ components/mpas-albany-landice/src/build_options.mk
@@ -53,6 +53,9 @@ endif
 # Optional FastIsostasy model
 ifeq "$(FASTISOSTASY)" "true"
     override CPPFLAGS += -DUSE_FASTISOSTASY
+    FASTISOSTASY_LINK_LIBS += $(ROOT_DIR)/FastIsostasy/libisostasy/include/libisostasy.a
+    FASTISOSTASY_LINK_LIBS += $(ROOT_DIR)/fesm-utils/utils/include-serial/libfesmutils.a
+    FASTISOSTASY_LINK_LIBS += $(ROOT_DIR)/fesm-utils/fftw-serial/lib/libfftw3.a
 endif
 
 # ===================================
